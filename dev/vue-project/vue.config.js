11e0cf8d9c51b4c5dd48afab20d78355eb1e0189
wangyan15 <promiseid@126.com>
2019-07-10 16:29:20 +0800

    #init-

--- dev/vue-project/vue.config.js
+++ dev/vue-project/vue.config.js
@@ -4,52 +4,55 @@
 
 var path = require('path');
 var fs = require('fs');
-const SpritesmithPlugin = require("webpack-spritesmith"); //引入雪碧图
+var SpritesmithPlugin = require("webpack-spritesmith");
+
 const resolve = dir => path.join(__dirname, dir);
 
+const args = process.argv.splice(2);
 
-var args = process.argv.splice(2);
-var environment = args[0];
-var sourcePath = environment == 'build' ? args[5] : args[2];
-// console.log('----Arguments----',args,sourcePath)
-var inputPath = sourcePath.replace(/^(\S+\/)[vV]([0-9])[0-9.]{0,}$/img,'$1v$2');
-var dir = inputPath.split('/')[0];
-// console.log('------inputPath------',inputPath);
+//环境
+const environment = args[0] == 'build' ?
+    ( args.length == 6 && args[4] == 'prod' ? 'prod' : 'dev' ) :
+    'local';
 
-var enviroment = environment == 'build' ? ( args.length == 6 && args[4] == 'prod' ? 'prod' : 'dev' ) : 'dev';
-// 打包根目录名
-const rootDir = enviroment == 'prod' ? 'trunk' : 'branches';
+//打包相对目录 example => demo/v1.0.0
+const sourcePath = environment == 'local' ?
+    args[2] :
+    args[5];
 
-let dirs = [];
-let isScss = fs.readdirSync(path.resolve(__dirname,`src/${sourcePath}/assets`)).indexOf('scss')!=-1;
-if(isScss){
-    let url = path.resolve(__dirname,`src/${sourcePath}/assets/scss`);
-    let files = fs.readdirSync(url);
-    files.forEach(file=>{
-        let fileName = path.resolve(url, file);
-        let stat = fs.statSync(fileName);
-        if(stat.isFile() && /^\S+\.common.scss$/img.test(file)){
-            dirs.push(fileName);
-        }
-    });
-}
-// console.log('scss----',path.join(__dirname,`src/${sourcePath}`));
-// console.log('全局scss',dirs);
+//输出相对目录 example => demo/v1
+const outputPath = sourcePath.replace(/^(\S+\/)[vV]([0-9])[0-9.]{0,}$/img,'$1v$2');
+
+const projectName = outputPath.split('/')[0];
+
+//sass目录
+const dirSass = path.resolve(__dirname,`src/${sourcePath}/assets/scss`);
+
+// 打包开发根目录
+const rootDir = environment == 'prod' ?
+    'trunk' :
+    'branches';
+//打包根域名
+const rootDomain = environment == 'prod' ?
+    'http://res.leju.com' :
+    ( environment == 'dev' ? 'http://res.bch.leju.com' : '' );
+const outputDir = `../../${rootDir}/assets/app/vue/${outputPath}`;
+
 require(path.resolve(__dirname,`src/${sourcePath.split('/')[0]}/webpack.config.js`))
 var config = {
-    outputDir:`../../${rootDir}/assets/app/vue/${inputPath}`,
-    publicPath:'/assets/app/vue/'+inputPath+'/',
+    outputDir,
+    publicPath:`${rootDomain}/assets/app/vue/${outputPath}/`,
     indexPath :  'index.html',
     assetsDir : '',
     filenameHashing : false,
     lintOnSave : true,
     runtimeCompiler : true,
     pages : {
-        [dir] : {
+        [projectName] : {
             entry:'src/'+sourcePath+'/main.js',
             template:'src/'+sourcePath+'/public/index.html',
             filename : 'index.html',
-            chunks : ['chunk-vendors', 'chunk-common', dir]
+            chunks : ['chunk-vendors', 'chunk-common', projectName]
         }
     },
     devServer : {
@@ -67,7 +70,19 @@ var config = {
             .set("assets", resolve(`src/${sourcePath}/assets`))
             .end();
 
-        if(dirs.length>0){
+        //resource-sass-loader 预处理的sass文件
+        let resourceSassFiles = [];
+        if(fs.readdirSync(path.resolve(__dirname,`src/${sourcePath}/assets`)).indexOf('scss')!=-1){
+            let files = fs.readdirSync(dirSass);
+            files.forEach(file=>{
+                const fileName = path.resolve(dirSass, file);
+                let stat = fs.statSync(fileName);
+                if(stat.isFile() && /^\S+\.common.scss$/img.test(file)){
+                    resourceSassFiles.push(fileName);
+                }
+            });
+        }
+        if(resourceSassFiles.length>0){
             // 配置全局scss
             const oneOfsMap = config.module.rule('scss').oneOfs.store
             //insert loader
@@ -76,14 +91,13 @@ var config = {
                     .use('sass-resources-loader')
                     .loader('sass-resources-loader')
                     .options({
-                        resources: dirs
+                        resources: resourceSassFiles
                     })
                     .end()
             })
         }
 
         let targetPlatform = global.targetPlatform || ['original', 'mobile', 'pc'];
-        console.log('----=====targetPlatform====----',targetPlatform);
         // sprites
         // 模板 for pc
         const generateSpritePX = function (data) {
@@ -95,7 +109,7 @@ var config = {
 
 
             let escapedImage = sheet.escaped_image;
-            escapedImage = `/assets/app/vue/${inputPath}/img/${basename}.png`;
+            escapedImage = `${rootDomain}/assets/app/vue/${outputPath}/img/${basename}.png`;
             sheet.escaped_image = escapedImage;
             console.log('pc(px)',basename, x + 'x','雪碧图图片路径:',sheet.escaped_image);
 
@@ -127,7 +141,7 @@ var config = {
             let x = mArr ? mArr[1] : 1;
 
             let escapedImage = sheet.escaped_image;
-            escapedImage = `/assets/app/vue/${inputPath}/img/${basename}.png`;
+            escapedImage = `${rootDomain}/assets/app/vue/${outputPath}/img/${basename}.png`;
             sheet.escaped_image = escapedImage;
             console.log('mobile(rem)',basename, x + 'x','雪碧图图片路径:',sheet.escaped_image);
 
@@ -159,7 +173,7 @@ var config = {
                 spritesDirs.forEach(filename => {
                     let stat = fs.statSync(path.resolve(__dirname, `src/${sourcePath}/sprites`, filename));
                     if (stat.isDirectory()) {
-                        let conf = generateSpritesmith(filename, args);
+                        let conf = generateSpritesmith(filename);
                         confs.push(conf[0]);
                         ;((conf)=>{
                             config
@@ -176,57 +190,52 @@ var config = {
             }
         })();
 
-        function generateSpritesmith(filename, args){
-                let cwd = path.resolve(__dirname, `src/${sourcePath}/sprites/${filename}`);
-                let target = {
-                    image: path.resolve(__dirname, `../../${rootDir}/assets/app/vue/${inputPath}/img/sprite_${filename}.png`),
-                    css: []
-                };
-                if (targetPlatform.indexOf('original') !== -1) {
-                    target.css.push(
-                        // 默认生成的文件
-                        [path.resolve(__dirname, `src/${sourcePath}/assets/scss/sprite_${filename}o.scss`)]
-                    );
-                }
-                if (targetPlatform.indexOf('mobile') !== -1) {
-                    target.css.push(
-                        // rem scss
-                        [path.resolve(__dirname, `src/${sourcePath}/assets/scss/sprite_${filename}.rem.scss`), { format: 'generateSpriteREM' }]
-                    );
-                }
-                if (targetPlatform.indexOf('pc') !== -1) {
-                    target.css.push(
-                        // pc scss
-                        [path.resolve(__dirname, `src/${sourcePath}/assets/scss/sprite_${filename}.px.scss`), { format: 'generateSpritePX' }],
-                    );
-                }
-                let conf = {
-                    // 输入
-                    src: {
-                        cwd,
-                        glob: "*.png"
-                    },
-                    // 输出（css，sprites.png）
-                    target,
-                    customTemplates: { generateSpritePX, generateSpriteREM },
-                    // 样式文件中调用雪碧图地址写法
-                    // apiOptions: {
-                    //     cssImageRef: `src/${sourcePath}/assets/images/sprite_${filename}.png`
-                    // },
-                    spritesmithOptions: {
-                        algorithm: "binary-tree",
-                        padding:20
-                    },
-                };
-                return [conf];
+        function generateSpritesmith(filename){
+            let cwd = path.resolve(__dirname, `src/${sourcePath}/sprites/${filename}`);
+            let target = {
+                image: path.resolve(__dirname, outputDir, `img/sprite_${filename}.png`),
+                css: []
+            };
+            if (targetPlatform.indexOf('original') !== -1) {
+                target.css.push(
+                    // 默认生成的文件
+                    [path.resolve(dirSass, `sprite_${filename}o.scss`)]
+                );
             }
-
+            if (targetPlatform.indexOf('mobile') !== -1) {
+                target.css.push(
+                    // rem scss
+                    [path.resolve(dirSass, `sprite_${filename}.rem.scss`), { format: 'generateSpriteREM' }]
+                );
+            }
+            if (targetPlatform.indexOf('pc') !== -1) {
+                target.css.push(
+                    // pc scss
+                    [path.resolve(dirSass, `sprite_${filename}.px.scss`), { format: 'generateSpritePX' }],
+                );
+            }
+            return [{
+                // 输入
+                src: {
+                    cwd,
+                    glob: "*.png"
+                },
+                // 输出（css，sprites.png）
+                target,
+                customTemplates: { generateSpritePX, generateSpriteREM },
+                // 样式文件中调用雪碧图地址写法
+                // apiOptions: {
+                //     cssImageRef: `src/${sourcePath}/assets/images/sprite_${filename}.png`
+                // },
+                spritesmithOptions: {
+                    algorithm: "binary-tree",
+                    padding:20
+                }
+            }];
+        }
     }
 };
 
-
-// console.log(JSON.stringify(config))
-
-console.log('输出路径---->',path.resolve(`../../${rootDir}/assets/app/vue/${inputPath}`));
+console.log('输出路径---->',path.resolve(`../../${rootDir}/assets/app/vue/${outputPath}`));
 
 module.exports = config;
