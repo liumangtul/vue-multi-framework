ce286a24a7f1e2c6c9a6d538505ca659748e7136
wangyan15 <promiseid@126.com>
2019-07-16 18:12:42 +0800

    #init

--- dev/vue-project/src/demo/v1.0.0/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <header>
-        <h1>头部信息</h1>
-    </header>
-</template>
-
-
-<style scoped>
-</style>
--- dev/vue-project/src/demo/webpack.config.js
+++
@@ -1,8 +0,0 @@
-/**
- *@fileOverview webpack.config.js
- *@Author wangyan15@leju.com
- *@Date 2019/7/8 下午7:04
- *@template
- */
-
-// global.targetPlatform = ['mobile'];
